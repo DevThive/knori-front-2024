b52311209470113f9cf100f800b1a6cbd44228d2
DevThive <thive8564@gmail.com>
2024-03-19 19:52:11 +0900

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -13,9 +13,8 @@
 import Home2 from "./home-two/page";
 import ReactGA from "react-ga4";
 
-if (process.env.NEXT_PUBLIC_GA_ID) {
-  ReactGA.initialize(process.env.NEXT_PUBLIC_GA_ID);
-}
+ReactGA.initialize(process.env.NEXT_PUBLIC_GA_ID);
+ReactGA.send("pageview");
 
 export default function Home() {
   return (

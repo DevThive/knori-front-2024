d12387e17d57a5b011c223683cd4685d28f64b71
DevThive <thive8564@gmail.com>
2024-04-01 15:11:23 +0900

    Update deluxe.jsx

--- app/home-two/deluxe.jsx
+++ app/home-two/deluxe.jsx
@@ -1,21 +1,18 @@
-import classist from "@/components/data/class-all";
+import classlist from "@/components/data/class-all";
+import { useState, useEffect } from "react";
+import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useState } from "react";
 
-const Mainclass = () => {
+const Deluxe = () => {
   const [classes, setClasses] = useState([]); // 상태 추가
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await classist();
+      const data = await classlist();
       setClasses(data);
     };
     fetchData();
   }, []);
-
-  const handleImageError = (event) => {
-    event.target.src = "/img/blog/blog-3.jpg"; // 기본 이미지 URL로 변경
-  };
 
   const classItem = classes.slice(0, 3);
   return (
@@ -24,54 +21,53 @@ const Mainclass = () => {
         <div className="row align-items-center mb-30">
           <div className="col-xl-6 col-lg-6 col-md-8">
             <div className="deluxe__three-title">
-              <span className="subtitle__one">Healing Class</span>
-              <h2>클래스 소개</h2>
+              <span className="subtitle__one">Deluxe and Luxury</span>
+              <h2>Luxury Rooms</h2>
             </div>
           </div>
           <div className="col-xl-6 col-lg-6 col-md-4">
             <div className="deluxe__three-right">
-              <Link className="theme-btn" href="/class-list">
-                자세히보기<i className="fal fa-long-arrow-right"></i>
+              <Link className="theme-btn" href="/about">
+                Read More<i className="fal fa-long-arrow-right"></i>
               </Link>
             </div>
           </div>
         </div>
         <div className="row">
-          {classItem.map((data, index) => (
+          {classItem.map((item, index) => (
             <div key={index} className="col-xl-4 col-lg-6 col-md-6 mt-30">
-              <div
-                className={`deluxe__three-item ${index === 1 ? "center" : ""}`}
-              >
-                {console.log(data.photo)}
-                <div
-                  className="deluxe__three-item-image"
-                  style={{
-                    position: "relative",
-                    width: "100%",
-                    height: "400px",
-                  }}
-                >
-                  <img
-                    style={{ objectFit: "cover" }}
-                    src={data.photo}
-                    alt="image"
-                    onError={handleImageError} // 이미지 로드 실패 시 기본 이미지로 대체
+              <div className={`deluxe__three-item ${item.cl}`}>
+                <div className="deluxe__three-item-image">
+                  <Image
+                    src={item.photo}
+                    alt={item.title}
+                    width={500}
+                    height={500}
                   />
                   <div className="deluxe__three-item-image-content">
                     <h4>
-                      <a href={`/class-details/${data.id}`}>{data.title}</a>
+                      <a href="/room-details">{item.title}</a>
+                      <span>${item.price}/Night</span>
                     </h4>
-                    <h5>
-                      <span>25,000원/1인</span>
-                    </h5>
-
+                    <p>{item.des}</p>
+                    <div className="deluxe__three-item-image-content-meta">
+                      <ul>
+                        <li>
+                          <i className="fal fa-bed-alt"></i> ({item.bed}) bed's
+                        </li>
+                        <li>
+                          <i className="fal fa-users"></i> ({item.guest})
+                          Guest's
+                        </li>
+                      </ul>
+                    </div>
                     <div className="deluxe__three-item-image-content-bottom">
-                      <a
-                        className="simple-btn"
-                        href={`/class-details/${data.id}`}
-                      >
-                        <i className="far fa-chevron-right"></i> 자세히보기
-                      </a>
+                      <Link className="simple-btn" href="/room-details">
+                        <i className="far fa-chevron-right"></i> Read More
+                      </Link>
+                      <p>
+                        <i className="fas fa-star"></i> <span>4.8</span>2k
+                      </p>
                     </div>
                   </div>
                 </div>
@@ -84,4 +80,4 @@ const Mainclass = () => {
   );
 };
 
-export default Mainclass;
+export default Deluxe;

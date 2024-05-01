0b2ea0f51a5e9b2eac4d371d59a016af7ebe258a
DevThive <thive8564@gmail.com>
2024-05-02 03:09:57 +0900

    클래스 && 공지사항 상세보기 수정

--- app/class-details/[id]/blog-comment.jsx
+++ app/class-details/[id]/blog-comment.jsx
@@ -31,52 +31,7 @@ const Blogcomment = ({ classData }) => {
           alt=""
         />
         <div className="blog__details-left-meta"></div>
-        <p className="mb-25">{classData.content}</p>
-        {/* <p>
-          Nulla at eleifend lorem. Praesent et ex sed metus egestas feugiat.
-          Donec velit libero, feugiat ac dictum vel, dignissim id ante. Praesent
-          hendrerit posuere condimentum..
-        </p>
-        <div className="blog__details-left-box">
-          <div className="blog__details-left-box-icon">
-            <img src="/img/icon/quote.png" alt="" />
-          </div>
-          <p>
-            Aenean imperdiet finibus sodales. Sed non ex nisl. Maecenas ut
-            dictum neque, at euismod felis. Etiam rhoncus neque vitae efficitur
-            mollis. Vestibulum sed pulvinar magna. Suspendisse
-          </p>
-          <h5>David Beckham</h5>
-        </div>
-        <p>
-          Vestibulum eget tellus rhoncus, dictum massa a, mattis massa. Cras in
-          leo semper, ultricies ligula nec, ornare tellus. Suspendisse quam
-          risus, semper et ultricies a, commodo eu tortor. Phasellus elementum
-          tincidunt varius. Nam facilisis, ante eget gravida vestibulum, ante
-          nisi feugiat nulla, in dapibus neque turpis et dolor. Vestibulum in
-          urna urna.
-        </p>
-        <div className="row mt-40 mb-40">
-          <div className="col-sm-6 sm-mb-30">
-            <div className="blog__details-left-list">
-              <img className="img__full" src="/img/blog/blog-11.jpg" alt="" />
-            </div>
-          </div>
-          <div className="col-sm-6">
-            <div className="blog__details-left-list blog__details-left-list-hover">
-              <img className="img__full" src="/img/blog/blog-12.jpg" alt="" />
-            </div>
-          </div>
-        </div>
-        <p className="mb-30">
-          Design pretium fermentum quam, sit amet cursus ante sollicitudin vel.
-          Morbi consequat risus consequat, porttitor orci sit amet, iaculis
-          nisl. Integer quis sapien neceli ultrices euismod sit amet id lacus.
-          Sed a imperdiet erat. Duis eu est dignissim lacus dictum hendrerit
-          quis vitae mi. Fusce eu nulla ac nisi cursus tincidun. Interdum et
-          malesuada fames ac ante ipsum primis in faucibus. Integer tristique
-          sem eget leo faucibus porttitor.
-        </p> */}
+        <div dangerouslySetInnerHTML={{ __html: classData.content }} />
       </div>
     </div>
   );

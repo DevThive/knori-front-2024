983bc76761a571dbdfbfbddf2330f9a3e1d8dbb9
DevThive <thive8564@gmail.com>
2024-05-08 01:22:10 +0900

    문의하기 기능 ( 비번, 답변확인 & 문의하기 )

--- app/contact/contact.jsx
+++ app/contact/contact.jsx
@@ -2,18 +2,25 @@
 import React, { useState, useEffect } from "react";
 import instance from "../axios/axiosInstance";
 import InquiryModal from "./contact_modal"; // 경로 확인 필요
+import axios from "axios";
+import { Card } from "@mui/material";
 
 const contact = () => {
   const [showModal, setShowModal] = useState(false);
   const [inquiries, setInquiries] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState(10);
+  const [openInquiryId, setOpenInquiryId] = useState(null); // 추가
+  const [showPasswordModal, setShowPasswordModal] = useState(false);
+  const [password, setPassword] = useState("");
+  const [pwModalId, setpwModalId] = useState(null);
 
   useEffect(() => {
     const fetchInquiries = async () => {
       try {
         const response = await instance.get("/contact/contactlists");
         setInquiries(response.data);
+        console.log(response.data);
       } catch (error) {
         console.error("문의 리스트를 불러오는데 실패했습니다.", error);
       }
@@ -21,6 +28,70 @@ const contact = () => {
 
     fetchInquiries();
   }, []);
+
+  const toggleInquiryDetails = (inquiry) => {
+    if (inquiry.public === 1) {
+      // public 상태가 1이면 비밀번호 모달을 띄운다.
+      if (openInquiryId === inquiry.id) {
+        setOpenInquiryId(null); // 이미 열려있다면 닫기
+      } else {
+        setShowPasswordModal(true);
+        setpwModalId(inquiry.id); // 닫혀있다면 열기
+      }
+    } else {
+      // public 상태가 1이 아니라면 기존 로직을 수행한다.
+      if (openInquiryId === inquiry.id) {
+        setOpenInquiryId(null); // 이미 열려있다면 닫기
+      } else {
+        setOpenInquiryId(inquiry.id); // 닫혀있다면 열기
+      }
+    }
+  };
+
+  // const toggleInquiryDetails = (id) => {
+  //   if (openInquiryId === id) {
+  //     setOpenInquiryId(null); // 이미 열려있다면 닫기
+  //   } else {
+  //     setOpenInquiryId(id); // 닫혀있다면 열기
+  //   }
+  // };
+
+  //비밀번호 확인 로직
+  const checkPassword = async (id) => {
+    // 비밀번호 검증 로직 구현, 예시는 간단히 비교합니다.
+    // 실제로는 서버에 비밀번호가 맞는지 확인하는 요청을 보내야 합니다.
+    console.log(password);
+    try {
+      const response = await instance.post(`/contact/password/${id}`, {
+        password: password,
+      });
+
+      // console.log(response);
+
+      if (response.data == true) {
+        setShowPasswordModal(false);
+        // 비밀번호가 맞다면 답변 세부 정보를 보여줄 수 있도록 처리
+        // console.log(id);
+        setOpenInquiryId(id);
+        setPassword("");
+      } else {
+        alert("비밀번호가 틀렸습니다.");
+        setPassword("");
+      }
+    } catch (error) {
+      console.log("서버에 오류가 있습니다.");
+    }
+    // // 비밀번호가 틀리면 사용자에게 알림}
+    // if (password === "1234") {
+    //   setShowPasswordModal(false);
+    //   // 비밀번호가 맞다면 답변 세부 정보를 보여줄 수 있도록 처리
+    //   console.log(id);
+    //   setOpenInquiryId(id);
+    //   setPassword("");
+    // } else {
+
+    // }
+  };
 
   const handleOpenModal = () => {
     setShowModal(true);
@@ -30,11 +101,24 @@ const contact = () => {
     setShowModal(false);
   };
 
-  const handleSubmitInquiry = async (inquiry) => {
+  const handleSubmitInquiry = async (
+    inquiry,
+    name,
+    phone,
+    title,
+    isPrivate,
+    password
+  ) => {
+    console.log(inquiry, name, phone, title, isPrivate, password);
     try {
       // 주의: 여기도 `instance.post`로 변경해야 할 수 있습니다.
-      const response = await instance.post("/contact/contactlists", {
+      const response = await instance.post("contact/addcontact", {
+        content_title: title,
         content: inquiry,
+        user_name: name,
+        user_phone: phone,
+        public: isPrivate,
+        password: password,
       });
       setInquiries([...inquiries, response.data]); // 응답 구조에 따라 조정 필요
       setShowModal(false);
@@ -60,53 +144,158 @@ const contact = () => {
   };
 
   return (
-    <div>
-      <button onClick={handleOpenModal}>문의하기</button>
-      <InquiryModal
-        show={showModal}
-        onClose={handleCloseModal}
-        onSubmit={handleSubmitInquiry}
-      />
+    <div style={{ width: "80%", margin: "auto", outline: "none" }}>
       <div>
-        <h2>문의 리스트</h2>
-        {/* 헤더 */}
         <div
           style={{
             display: "flex",
-            padding: "10px",
-            background: "#f0f0f0",
-            fontWeight: "bold",
+            justifyContent: "flex-end", // 버튼을 오른쪽으로 정렬합니다.
+            alignItems: "center",
+            padding: "1%",
           }}
         >
-          <div style={{ flex: 1 }}>ID</div>
-          <div style={{ flex: 3 }}>문의 제목</div>
-          <div style={{ flex: 2 }}>작성자</div>
-          <div style={{ flex: 2 }}>날짜</div>
+          <button onClick={handleOpenModal}>문의하기</button>
         </div>
-        {/* 문의 리스트 */}
-        {currentInquiries.map((inquiry) => (
+        <InquiryModal
+          show={showModal}
+          onClose={handleCloseModal}
+          onSubmit={handleSubmitInquiry}
+        />
+        <div>
+          {/* 헤더 */}
           <div
-            key={inquiry.id}
             style={{
               display: "flex",
-              borderBottom: "1px solid #ddd",
               padding: "10px",
+              background: "#f0f0f0",
+              fontWeight: "bold",
             }}
           >
-            <div style={{ flex: 1 }}>{inquiry.id}</div>
-            <div style={{ flex: 3 }}>{inquiry.content_title}</div>
-            <div style={{ flex: 2 }}>{inquiry.user_name}</div>
-            <div style={{ flex: 2 }}>{inquiry.createdAt}</div>
+            <div style={{ flex: 1 }}>ID</div>
+            <div style={{ flex: 5 }}>문의 제목</div>
+            <div style={{ flex: 2 }}>작성자</div>
+            <div style={{ flex: 2 }}>날짜</div>
+            <div style={{ flex: 1 }}>상태</div>
           </div>
-        ))}
+          {/* 문의 리스트 */}
+          {currentInquiries.map((inquiry) => (
+            <React.Fragment key={inquiry.id}>
+              <div
+                key={inquiry.id}
+                style={{
+                  display: "flex",
+                  borderBottom: "1px solid #ddd",
+                  padding: "10px",
+                  cursor: "pointer", // 커서 포인터 추가
+                }}
+                onClick={() => toggleInquiryDetails(inquiry)} // 클릭 이벤트 추가
+              >
+                <div style={{ flex: 1 }}>{inquiry.id}</div>
+                <div style={{ flex: 5 }}>{inquiry.content_title}</div>
+                <div style={{ flex: 2 }}>{inquiry.user_name}</div>
+                <div style={{ flex: 2 }}>
+                  {new Date(inquiry.createdAt).toLocaleDateString("ko-KR", {
+                    year: "numeric",
+                    month: "2-digit",
+                    day: "2-digit",
+                  })}
+                </div>
+                <div style={{ flex: 1 }}>
+                  {inquiry.contact_answer === "" ? "미답변" : "답변완료"}
+                </div>
+              </div>
+
+              {openInquiryId === inquiry.id && (
+                <div
+                  style={{
+                    padding: "20px",
+                    background: "#fff",
+                    borderRadius: "8px",
+                    boxShadow: "0 2px 4px rgba(0,0,0,0.1)",
+                    marginTop: "10px",
+                    marginBottom: "10px",
+                    border: "1px solid #e2e8f0",
+                  }}
+                >
+                  {/* 답변 내용이 있을 경우 답변 내용 렌더링, 없으면 대기 메시지 렌더링 */}
+                  {inquiry.contact_answer.trim() ? (
+                    <>
+                      <h3 style={{ fontSize: "18px", color: "#333" }}>답변</h3>
+                      <p
+                        style={{
+                          fontSize: "16px",
+                          color: "#333",
+                          lineHeight: "1.5",
+                        }}
+                      >
+                        {inquiry.contact_answer}
+                      </p>
+                    </>
+                  ) : (
+                    <p
+                      style={{
+                        fontSize: "16px",
+                        color: "#333",
+                        lineHeight: "1.5",
+                      }}
+                    >
+                      미답변 상태입니다.{" "}
+                      <span>
+                        기다려 주시면 최대한 빠르게 답변 드리겠습니다.
+                      </span>
+                    </p>
+                  )}
+                </div>
+              )}
+            </React.Fragment>
+          ))}
+        </div>
+        {/* 페이지네이션 컨트롤 */}
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            margin: "20px 0",
+          }}
+        >
+          <button style={{ margin: "1%" }} onClick={prevPage}>
+            &lt; 이전
+          </button>
+          <button style={{ margin: "1%" }} onClick={nextPage}>
+            다음 &gt;
+          </button>
+        </div>
       </div>
-      {/* 페이지네이션 컨트롤 */}
-      <div
-        style={{ display: "flex", justifyContent: "center", margin: "20px 0" }}
-      >
-        <button onClick={prevPage}>&lt; 이전</button>
-        <button onClick={nextPage}>다음 &gt;</button>
-      </div>
+      {showPasswordModal && (
+        <div
+          style={{
+            position: "fixed",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            backgroundColor: "white",
+            padding: "20px",
+            borderRadius: "10px",
+            boxShadow: "0 2px 4px rgba(0,0,0,0.2)",
+          }}
+        >
+          {/* 비밀번호 입력 모달 UI 구현 */}
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="비밀번호"
+          />
+          <button
+            onClick={() => {
+              checkPassword(pwModalId); // 여기서 문제가 발생할 수 있습니다.
+              setShowPasswordModal(false);
+            }}
+          >
+            확인
+          </button>
+        </div>
+      )}
     </div>
   );
 };

a70bf59b1592359e5751108d8906659c9e38b6ab
DevThive <thive8564@gmail.com>
2024-06-22 00:12:38 +0900

    갤러리 부분 디자인 수정

--- app/gallery/current-blog-items.jsx
+++ app/gallery/current-blog-items.jsx
@@ -1,34 +1,56 @@
+import Image from "next/image";
+
 const Currentblogitems = ({ currentBlogItems }) => {
   const extractTitle = (caption) => {
     return caption.replace(/#[^\s#]+/g, "").trim();
   };
+
+  const handleImageError = (event) => {
+    event.target.src = "/img/blog/blog-3.jpg"; // 기본 이미지 URL로 변경
+  };
   return (
-    <div className="container">
-      <div className="row">
-        {currentBlogItems?.map((data, id) => (
-          <>
-            <div className="col-xl-4 col-lg-6 col-md-6 mb-30" key={id}>
-              <div className="blog__two-item">
-                <div className="blog__two-item-image">
-                  <img src={data.media_url} alt="image" />
-                  {/* <div className="blog__two-item-image-date">
-                    <h5>{data.id}</h5>
-                    <span>{data.id}</span>
-                  </div> */}
-                </div>
-                <div className="blog__two-item-content">
-                  {/* <h6>Post by - {data.id}</h6> */}
-                  <h4>
-                    <a href={data.permalink}>{extractTitle(data.caption)}</a>
-                  </h4>
-                  <a className="simple-btn" href={data.permalink}>
-                    <i className="far fa-chevron-right"></i>자세히보기
-                  </a>
+    <div className="modern__room section-padding">
+      <div className="container">
+        <div className="row">
+          {currentBlogItems?.map((data, id) => (
+            <div key={id} className="col-xl-4 col-lg-6 col-md-6 mb-30">
+              <div className="deluxe__three-item">
+                <div
+                  className="deluxe__three-item-image"
+                  style={{
+                    position: "relative",
+                    width: "100%",
+                    height: "350px",
+                  }}
+                >
+                  <img
+                    src={data.media_url}
+                    alt="image"
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "cover",
+                      position: "absolute",
+                    }}
+                    onError={handleImageError} // onError 핸들러를 img 태그에 적용
+                  />
+
+                  <div className="deluxe__three-item-image-content">
+                    <h4>
+                      <a href={data.permalink}>{extractTitle(data.caption)}</a>
+                    </h4>
+
+                    <div className="deluxe__three-item-image-content-bottom">
+                      <a className="simple-btn" href={data.permalink}>
+                        <i className="far fa-chevron-right"></i> 자세히보기
+                      </a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
